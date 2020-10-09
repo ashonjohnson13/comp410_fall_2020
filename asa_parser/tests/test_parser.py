090955a2083e2c0d7129e2135e5f86c9d43719a8
Mike Claes <claesmk@gmail.com>
2020-10-09 17:39:40 -0400

    Added test_tech_support_license

--- asa_parser/tests/test_parser.py
+++ asa_parser/tests/test_parser.py
@@ -59,3 +59,7 @@ class ParserTest(unittest.TestCase):
 
     def test_startup_config_errors(self):
         self.assertEqual(True, True)
+
+    def test_tech_support_license(self):
+        self.assertEqual(True, True)
+
